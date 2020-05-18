8b4ff3d3f591a952e62ed80e5582aa7155bf88dd
Jayno17 <julia.nowak@gmail.com>
2020-05-18 20:47:01 +0100

    initial commit

---
+++ src/components/FilmTable.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Film from "./Film";
+
+class FilmTable extends Component{
+
+  render(){
+    const filmNodes = this.props.film.map((film) => {
+      return (
+        <Film name={film.name} url={film.url} key={film.id} /> 
+      );
+    });
+    return(
+      <div className="film-table">
+      <ul>
+        {filmNodes}
+        </ul>
+      </div>
+    );
+  }
+}
+export default FilmTable;
